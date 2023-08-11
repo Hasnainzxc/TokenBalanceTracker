6a8bfb647155712d94618af9774fbb6e468d88a9
hasnainzxc <hasnainzxc@icloud.com>
2023-08-11 04:53:28 -0700

    ui for app

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 from app.api.routes import balance
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
+from blockchain import get_balance
 
-app.include_router(balance.router, prefix="/api/v1")
+router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@router.get("/api/v1/balance")
+async def fetch_balance(wallet: str):
+    return get_balance(wallet)
